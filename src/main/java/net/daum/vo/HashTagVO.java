f3ff79bd8f33d38df5bb36ab77d161e2ace970b5
여준서 <duwnstj12@naver.com>
2024-05-26 23:45:00 +0900

    hashtag 엔티티빈 생성

--- src/main/java/net/daum/vo/HashTagVO.java
+++ src/main/java/net/daum/vo/HashTagVO.java
@@ -1,9 +1,34 @@
 package net.daum.vo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Setter
+@Getter
+@ToString
+@Entity
+@Table(name= "hashTag")
 public class HashTagVO {
-
+	
+	@Id //기본키
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name="mate_no")
+	private Community_boardVO mate_no;
+	
+	private String contry;
+	private String select_style;
+	
 	
 }
