a7740a5a24eeaf90a24ec4f8559cacfc14f4dc80
여준서 <duwnstj12@naver.com>
2024-05-27 00:53:36 +0900

    오류 발생해서 그 전 커밋으로 돌림

--- src/main/java/net/daum/vo/Community_boardVO.java
+++ src/main/java/net/daum/vo/Community_boardVO.java
@@ -1,7 +1,8 @@
 package net.daum.vo;
+
+
+
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,7 +11,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -27,8 +27,7 @@ import lombok.ToString;
 @ToString
 @Entity
 @SequenceGenerator(// 오라클 시퀀스 생성기
-		name = "mate_noseq_gename", 
-		sequenceName = "mate_no_seq", // 시퀀스 이름
+		name = "mate_noseq_gename", sequenceName = "mate_no_seq", // 시퀀스 이름
 		initialValue = 1, // 시퀀스 시작값
 		allocationSize = 1
 
@@ -41,24 +40,21 @@ public class Community_boardVO {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "mate_noseq_gename")
 	private Long mate_no;// 기본키
 
-	@OneToMany(mappedBy = "mate_no")
-	private List<HashTagVO> hashtags = new ArrayList<>(); 
-
 	@ManyToOne
-	@JoinColumn(name = "member_no")
+	@JoinColumn(name="member_no")
 	private DermyMemberVO member_no;
-
+	
 	private int mate_hit;
 	private String mate_title;
-
-	@Column(length = 4000)
+	
+	@Column(length=4000)
 	private String mate_cont;
-
-	@CreationTimestamp // 하이버네이트 기능으로 등록시점 날짜를 기록
-	private Timestamp makedate;// 등록 날짜
-
+	
+	@CreationTimestamp //하이버네이트 기능으로 등록시점 날짜를 기록
+	private Timestamp makedate;//등록 날짜
+	
 	@UpdateTimestamp
-	private Timestamp updatedate;// 하이버네이트 기능으로 업데이트 날짜 자동 기록
+	private Timestamp updatedate;//하이버네이트 기능으로 업데이트 날짜 자동 기록
 	private int mate_matching;
 	private String mate_sumnail;
 	private int mate_postuse;
--- src/main/java/net/daum/vo/DermyMemberVO.java
+++ src/main/java/net/daum/vo/DermyMemberVO.java
@@ -1,6 +1,8 @@
 package net.daum.vo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Getter;
