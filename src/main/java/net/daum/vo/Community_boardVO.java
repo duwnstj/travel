2ab0dcb0d12a71ea87e974106310b1fa7a5934e8
여준서 <duwnstj12@naver.com>
2024-05-29 23:46:04 +0900

    내일 다시 도전

--- src/main/java/net/daum/vo/Community_boardVO.java
+++ src/main/java/net/daum/vo/Community_boardVO.java
@@ -55,20 +55,16 @@ public class Community_boardVO {
 	
 	@UpdateTimestamp
 	private Timestamp updatedate;//하이버네이트 기능으로 업데이트 날짜 자동 기록
-	
-	private int mate_matching; //매칭상태 매칭된상태면 1, 매칭 안된 상태면 2
 	private String mate_sumnail; //썸네일(이미지 썸네일)
 	private int mate_limited; //제한인원 설정
 	private String mt_hashtag; //해시태그
-	private int bbs_ref;//원본글과 답변글을 묶어주는 글 그룹번호 역할
-	private int bbs_level; //답변글 정렬 순서
 	
 	/*
 	 * @OneToMany(mappedBy="cm_board",cascade=CascadeType.ALL,fetch=FetchType.EAGER)
 	 * private List<Cm_replVO> repls;//외래키로 댓글창에 기본키 참조
 	 */	
 	
-	@OneToMany(mappedBy="cm_board",cascade=CascadeType.ALL,fetch=FetchType.LAZY)
+	@OneToMany(mappedBy="community_Board",cascade=CascadeType.ALL,fetch=FetchType.LAZY)
 	private List<Cm_ImgVO> images;//외래키로 게시판 이미지 테이블에 기본키 참조
 
 }
